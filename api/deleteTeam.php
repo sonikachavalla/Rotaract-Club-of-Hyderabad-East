a1590c8c7c792aec7bc7432b1d35a4b286f3eb9b
sonikachavalla <sonika.chavalla@gmail.com>
2026-07-31 04:07:12 +0530

    setting up teams

--- api/deleteTeam.php
+++ api/deleteTeam.php
@@ -1,40 +1,11 @@
-<?php
+await window._fb.deleteDoc(
+    window._fb.doc(
+        window._fb.db,
+        "team",
+        pendingDelete.id
+    )
+);
 
-require "db.php";
+toast("Team member deleted!");
 
-header("Content-Type: application/json");
-
-$data = json_decode(file_get_contents("php://input"), true);
-
-if (!$data || empty($data["id"])) {
-    echo json_encode([
-        "success" => false,
-        "message" => "Invalid request"
-    ]);
-    exit;
-}
-
-$id = (int)$data["id"];
-
-$stmt = $conn->prepare("DELETE FROM team WHERE id=?");
-$stmt->bind_param("i", $id);
-
-if ($stmt->execute()) {
-
-    echo json_encode([
-        "success" => true
-    ]);
-
-} else {
-
-    echo json_encode([
-        "success" => false,
-        "message" => $stmt->error
-    ]);
-
-}
-
-$stmt->close();
-$conn->close();
-
-?>
+await loadTeam();
